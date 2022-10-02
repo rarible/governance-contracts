962e7eba699ecb01d21b12d5367423b248624d3d
disenotov <mitri4cool@mail.ru>
2022-10-02 17:58:56 +0300

    feat: get rid of staking in tests

--- test/Governance.test.js
+++ test/Governance.test.js
@@ -1,92 +1,56 @@
 
 const TestERC20 = artifacts.require("TestERC20.sol");
-const RariGovernor = artifacts.require("RariGovernor")
 const RariGovernorTest = artifacts.require("RariGovernorTest")
 
 const RariTimelockController = artifacts.require("RariTimelockController")
-const Staking = artifacts.require("Staking")
+
+const { expectThrow } = require("@daonomic/tests-common");
 
 contract("Governance", accounts => {
-  let erc20;
-  let staking;
+  let token;
   let governorTest;
   let timelock;
 
   let epochSize;
   
 	before(async () => {
-    erc20 = await TestERC20.new();
-
-    staking = await Staking.new()
-    await staking.__Staking_init(erc20.address, 0)
+    token = await TestERC20.new();
 
     timelock = await RariTimelockController.new()
-    await timelock.__RariTimelockController_init(0, [], [])
+    await timelock.__RariTimelockController_init(2, [], [])
     
     governorTest = await RariGovernorTest.new()
-    await governorTest.__RariGovernor_init(staking.address, timelock.address)
+    await governorTest.__RariGovernor_init(token.address, timelock.address)
 
     const PROPOSER_ROLE = await timelock.PROPOSER_ROLE()
     const EXECUTOR_ROLE = await timelock.EXECUTOR_ROLE();
     await timelock.grantRole(PROPOSER_ROLE, governorTest.address)
     await timelock.grantRole(EXECUTOR_ROLE, governorTest.address)
 
-    epochSize = await staking.WEEK()
+    epochSize = await token.WEEK()
 
-    console.log("delyay", (await governorTest.votingDelay()).toString())
-    console.log("block", (await governorTest.getBLock()).toString())
-    console.log()
-
-    await skipEpoch(epochSize)
+    await skipEpoch()
 	})
 
-	describe("governance", () => {
+	describe("governance", () => {    
     
-    it ("delay", async () => {
-      /*
-      console.log("delyay", (await governorTest.votingDelay()).toString())
-    console.log("block", (await governorTest.getBLock()).toString())
-    console.log()
-
-
-      await skipEpoch(epochSize)
-
-      console.log("delyay", (await governorTest.votingDelay()).toString())
-    console.log("block", (await governorTest.getBLock()).toString())
-    console.log()
-    */
-
-      
-    })
-    
-    
-    it ("proposal", async () => {
+    it("proposal works", async () => {
       
       const voter1 = accounts[1]
       const voter2 = accounts[2]
 
-      //minting and staking 1000 tokens voter1
-      await erc20.mint(voter1, 1000);
-   		await erc20.approve(staking.address, 1000, { from: voter1 });
-			await staking.stake(voter1, voter1, 1000, 1000, 103, { from: voter1 }); //address account, address delegate, uint amount, uint slope, uint cliff
-
-			assert.equal(await erc20.balanceOf(staking.address), 1000);				//balance Lock on deposite
-  		assert.equal(await erc20.balanceOf(voter1), 0);			      //tail user balance
-      assert.equal(await staking.balanceOf(voter1), 1000);  
+      //minting 1000 tokens voter1
+      await token.mint(voter1, 1000);
+			assert.equal(await token.balanceOf(voter1), 1000);
       
-      //minting 2000 and staking 1000 tokens voter2
-      await erc20.mint(voter2, 2000);
-   		await erc20.approve(staking.address, 1000, { from: voter2 });
-			await staking.stake(voter2, voter2, 1000, 1000, 103, { from: voter2 }); //address account, address delegate, uint amount, uint slope, uint cliff
-
-			assert.equal(await erc20.balanceOf(staking.address), 2000);				//balance Lock on deposite
-  		assert.equal(await erc20.balanceOf(voter2), 1000);			      //tail user balance
-      assert.equal(await staking.balanceOf(voter2), 1000);  
+      //minting 2000 tokens voter2
+      await token.mint(voter2, 2000);
+			assert.equal(await token.balanceOf(voter2), 2000);
 
       //transfer tokens to timelock
-      await erc20.transfer(timelock.address, 1000, {from: voter2})
-      assert.equal(await erc20.balanceOf(voter2), 0);		
-      assert.equal(await erc20.balanceOf(timelock.address), 1000);		
+      await token.transfer(timelock.address, 1000, {from: voter2})
+      assert.equal(await token.balanceOf(voter2), 1000);		
+      assert.equal(await token.balanceOf(timelock.address), 1000);		
 
       //governance
 
@@ -100,12 +64,11 @@ contract("Governance", accounts => {
 
       //console.log(await governorTest.getBLock())
       const tx = await governorTest.propose(
-        [erc20.address],
+        [token.address],
         [0],
         [transferCalldata],
         "Proposal #1: Give grant to team"
       );
-      const startBlock = tx.receipt.blockNumber;
 
       const ProposalCreated = await governorTest.getPastEvents("ProposalCreated", {
         fromBlock: tx.receipt.blockNumber,
@@ -113,6 +76,7 @@ contract("Governance", accounts => {
       });
 
       const proposalId = (ProposalCreated[0].returnValues.proposalId)
+      const proposal = await governorTest.proposals(proposalId)
 
       const VoteType = {
         Against: 0,
@@ -120,64 +84,74 @@ contract("Governance", accounts => {
         Abstain: 2
       }
 
-      await skipEpoch(50)
+      await moveToBLock(proposal.startBlock)
 
       //console.log(await governorTest.getBLock())
       
       await governorTest.castVote(proposalId, VoteType.For, {from: voter1})
       await governorTest.castVote(proposalId, VoteType.For, {from: voter2})
       
-      console.log(await governorTest.proposals(proposalId))
-      console.log(await governorTest.quorum(startBlock + 50)) 
-      
-      await moveToDeadLine(proposalId)
+      await moveToBLock(proposal.endBlock)
 
-      assert.equal(await erc20.balanceOf(user), 0)
+      assert.equal(await token.balanceOf(user), 0)
 
       const hashDiscr = await governorTest.hashDescription("Proposal #1: Give grant to team")
 
       await governorTest.queue(
-        [erc20.address],
+        [token.address],
         [0],
         [transferCalldata],
         hashDiscr
+      );
+
+      await expectThrow(
+        governorTest.execute(
+          [token.address],
+          [0],
+          [transferCalldata],
+          hashDiscr
+        )
       );
 
       //console.log(await timelock.getTimestamp(proposalId))
       await new Promise((resolve) => setTimeout(resolve, 1000 * 3))
 
       await governorTest.execute(
-        [erc20.address],
+        [token.address],
         [0],
         [transferCalldata],
         hashDiscr
       );
       
 
-      assert.equal(await erc20.balanceOf(user), 1000)
+      assert.equal(await token.balanceOf(user), 1000)
       
+    })
+
+    it("cancel", async () => {
+    
     })
     
 	})
 
-  async function moveToDeadLine(proposalId) {
+  async function moveToBLock(block) {
     let now = await governorTest.getBLock();
-    const deadline = (await governorTest.proposals(proposalId)).endBlock;
-    console.log("was:", now)
-    for (now; now <= deadline; now ++){
+    console.log(`moving to block ${block}`)
+    console.log("was:", now.toString())
+    for (now; now <= Number(block); now ++){
       await governorTest.incrementBlock();
     }
-    console.log("is:", await governorTest.getBLock())
+    console.log("is:", (await governorTest.getBLock()).toString())
+    console.log()
   }
 
-  async function skipEpoch(epochSize) {
+  async function skipEpoch() {
     console.log("was block", (await governorTest.getBLock()).toString())
     for (let i = 0; i < epochSize; i ++){
       await governorTest.incrementBlock();
     }
     console.log("now block", (await governorTest.getBLock()).toString())
+    console.log()
   }
-
-
 
 })
--- test/contracts/TestERC20.sol
+++ test/contracts/TestERC20.sol
@@ -8,4 +8,31 @@ contract TestERC20 is ERC20Upgradeable {
     function mint(address to, uint amount) external {
         _mint(to, amount);
     }
+
+    function WEEK() external pure returns (uint) {
+        return 50;
+    }
+
+    /**
+     * @dev Returns the current amount of votes that `account` has.
+     */
+    function getVotes(address account) external view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    /**
+     * @dev Returns the amount of votes that `account` had
+     * at the end of the last period
+     */
+    function getPastVotes(address account, uint256 blockNumber) external view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    /**
+     * @dev Returns the total supply of votes available 
+     * at the end of the last period
+     */
+    function getPastTotalSupply(uint256 blockNumber) external view returns (uint256) {
+        return totalSupply();
+    }
 }
