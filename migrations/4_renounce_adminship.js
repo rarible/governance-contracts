9b380d7cf03afa81692ee499ea8f4fa30fc69379
disenotov <mitri4cool@mail.ru>
2022-11-18 09:16:42 +0200

    chore: fix formatting

--- migrations/4_renounce_adminship.js
+++ migrations/4_renounce_adminship.js
@@ -3,27 +3,27 @@ const RariGovernor = artifacts.require("RariGovernor")
 const ProxyAdmin = artifacts.require("ProxyAdmin")
 
 const mainnet = {
-	adminProxyAddress: "0xDc8BaA86f136F8B0851F090a4DfFDc7b5F46688D",
+  adminProxyAddress: "0xDc8BaA86f136F8B0851F090a4DfFDc7b5F46688D",
 }
 const goerli = {
-	adminProxyAddress: "0x919AEd466F30A821670b12aaab3A4102d8536486",
+  adminProxyAddress: "0x919AEd466F30A821670b12aaab3A4102d8536486",
 }
 const def = {
-	adminProxyAddress: "0x0000000000000000000000000000000000000000",
+  adminProxyAddress: "0x0000000000000000000000000000000000000000",
 }
 
 let settings = {
-	"default": def,
-	"mainnet": mainnet,
-	"goerli": goerli
+  "default": def,
+  "mainnet": mainnet,
+  "goerli": goerli
 };
 
 function getSettings(network) {
-	if (settings[network] !== undefined) {
-		return settings[network];
-	} else {
-		return settings["default"];
-	}
+  if (settings[network] !== undefined) {
+    return settings[network];
+  } else {
+    return settings["default"];
+  }
 }
 
 module.exports = async function (deployer, network, accounts) {
@@ -42,7 +42,7 @@ module.exports = async function (deployer, network, accounts) {
 
   //renouncing TIMELOCK_ADMIN_ROLE role  
   const TIMELOCK_ADMIN_ROLE = await timeLock.TIMELOCK_ADMIN_ROLE()
-  await timeLock.renounceRole(TIMELOCK_ADMIN_ROLE, admin, {gas: 60000})
+  await timeLock.renounceRole(TIMELOCK_ADMIN_ROLE, admin, { gas: 60000 })
 
   //making timeLock admin of it's own (and governor's) ProxyAdmin contract
   const proxyAdmin = await ProxyAdmin.at(adminProxyAddress)
